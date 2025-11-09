0cfd2379384afd37b860e9bf33243cbfd59607f7
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-11-09 16:19:18 +0100

    added redux logic to scrum board

--- src/components/scrum/Scrum.tsx
+++ src/components/scrum/Scrum.tsx
@@ -1,8 +1,40 @@
 import styles from "./Scrum.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import {
+  addTicket,
+  toDoing,
+  toReview,
+  completeTodo,
+  deleteTodo,
+} from "../../features/scrum/scrumSlice";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { FiPlusCircle } from "react-icons/fi";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegCircleCheck } from "react-icons/fa6";
 
 const Scrum = () => {
   const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector((state) => state.scrum.todos);
+  const todo = todos.filter((t) => t.status === "todo");
+  const doing = todos.filter((t) => t.status === "doing");
+  const review = todos.filter((t) => t.status === "review");
+  const done = todos.filter((t) => t.status === "done");
+  const id = Date.now().toString();
+
+  const handleAdd = () => {
+    if (!inputValue.trim()) return;
+    dispatch(addTicket({ id, text: inputValue }));
+    setInputValue("");
+    inputRef?.current?.focus();
+  };
+
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleAdd();
+    }
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -16,20 +48,81 @@ const Scrum = () => {
             placeholder="Add your to-do"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleEnter}
+            ref={inputRef}
           />
           <div className={styles.buttons}>
-            <button className={styles.add}>Add</button>
+            <button className={styles.add} onClick={handleAdd}>
+              Add
+            </button>
           </div>
-          <div className={styles.todo}></div>
+          <div className={styles.todo}>
+            {todo.map((t) => (
+              <div className={styles.doing}>
+                <h3>{t.text}</h3>
+                <div className={styles.icons}>
+                  <FiPlusCircle
+                    className={styles.checkIcon}
+                    onClick={() => dispatch(toDoing(t.id))}
+                  />
+                  <FaRegTrashAlt
+                    className={styles.trashIcon}
+                    onClick={() => dispatch(deleteTodo(t.id))}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className={styles.col2}>
           <h1 className={styles.title}>DOING</h1>
+          {doing.map((t) => (
+            <div className={styles.doing}>
+              <h3>{t.text}</h3>
+              <div className={styles.icons}>
+                <FaRegCircleCheck
+                  className={styles.checkIcon}
+                  onClick={() => dispatch(toReview(t.id))}
+                />
+                <FaRegTrashAlt
+                  className={styles.trashIcon}
+                  onClick={() => dispatch(deleteTodo(t.id))}
+                />
+              </div>
+            </div>
+          ))}
         </div>
         <div className={styles.col3}>
           <h1 className={styles.title}>REVIEW</h1>
+          {review.map((t) => (
+            <div className={styles.doing}>
+              <h3>{t.text}</h3>
+              <div className={styles.icons}>
+                <FaRegCircleCheck
+                  className={styles.checkIcon}
+                  onClick={() => dispatch(completeTodo(t.id))}
+                />
+                <FaRegTrashAlt
+                  className={styles.trashIcon}
+                  onClick={() => dispatch(deleteTodo(t.id))}
+                />
+              </div>
+            </div>
+          ))}
         </div>
         <div className={styles.col4}>
           <h1 className={styles.title}>DONE</h1>
+          {done.map((t) => (
+            <div className={styles.doing}>
+              <h3>{t.text}</h3>
+              <div className={styles.icons}>
+                <FaRegTrashAlt
+                  className={styles.trashIcon}
+                  onClick={() => dispatch(deleteTodo(t.id))}
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/features/scrum/scrumSlice.ts
+++ src/features/scrum/scrumSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-type Status = "todo" | "doing" | "done";
+type Status = "todo" | "doing" | "review" | "done";
 
 type ScrumItem = {
   id: string;
@@ -17,10 +17,42 @@ const initialState: ScrumState = {
   todos: [],
 };
 
-const scrumToDo = createSlice({
+const scrumSlice = createSlice({
   name: "scrum",
   initialState,
-  reducers: {},
+  reducers: {
+    addTicket(state, action: PayloadAction<{ id: string; text: string }>) {
+      const { id, text } = action.payload;
+      state.todos.push({ id, text, status: "todo" });
+    },
+    toDoing(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const todo = state.todos.find((t) => t.id === id);
+      if (todo && todo.status === "todo") {
+        todo.status = "doing";
+      }
+    },
+    toReview(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const todo = state.todos.find((t) => t.id === id);
+      if (todo && todo.status === "doing") {
+        todo.status = "review";
+      }
+    },
+    completeTodo(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const todo = state.todos.find((t) => t.id === id);
+      if (todo && todo.status === "review") {
+        todo.status = "done";
+      }
+    },
+    deleteTodo(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      state.todos = state.todos.filter((d) => d.id !== id);
+    },
+  },
 });
 
-export default scrumToDo.reducer;
+export const { addTicket, toDoing, toReview, completeTodo, deleteTodo } =
+  scrumSlice.actions;
+export default scrumSlice.reducer;
