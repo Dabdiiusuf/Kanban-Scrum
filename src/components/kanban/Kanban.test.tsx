c083d364ad758afc06d6d5148ed7be29b6c14e60
Dabdiiusuf <daniel.abdinur@hotmail.com>
2025-11-12 11:30:23 +0100

    removed duplicate files

--- src/components/kanban/Kanban.test.tsx
+++
@@ -1,8 +0,0 @@
-import { renderWithProvider } from "../../test-utils";
-import Kanban from "./Kanban";
-
-describe("Kanban", () => {
-  it("render without crashing", () => {
-    renderWithProvider(<Kanban />);
-  });
-});
--- src/test-utils.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-
-export const renderWithProvider = (ui: React.ReactElement) =>
-  render(<Provider store={store}>{ui}</Provider>);
